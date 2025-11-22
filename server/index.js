7f414f414b2a6c7b4f0222b27f145ef74037edef
Abjura <107774277+Abjuraa@users.noreply.github.com>
2025-11-21 20:06:28 -0500

    refactor: se arreglo la ruta porque fallo al desplegar

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors({
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
